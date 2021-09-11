46d1df7f751f6862fa42a42bc2c170c3218d6a85
AlexBMJ <alexander.magno.jacobsen@gmail.com>
2021-09-11 19:26:59 +0200

    Implement and Test SplitLine and Resolution

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,8 +1,34 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
-namespace Assignment1.Tests
-{
-    public class RegExprTests
-    {
+namespace Assignment1.Tests {
+    public class RegExprTests {
+        [Theory]
+        [InlineData("Happy Go Lucky", "lower case test", "lower and UPPER", "1,2,3", "mixed 2>1=true")]
+        public void SplitLine_GivenListOfSentences_ReturnListOfWords(params string[] sentences) {
+            var result = RegExpr.SplitLine(sentences);
+            var expected = new string[] {
+                "Happy", "Go", "Lucky", "lower", "case", "test", "lower", "and", "UPPER", "1", "2", "3", "mixed", "2",
+                "1", "true"
+            };
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1920x1080\n1024x768, 800x600, 640x480\n320x200, 320x240, 800x600\n1280x960")]
+        public void Resolution_GivenString1920x1080_Return_ListOfTuple_1920_1080(string resolutionStringList) {
+            var result = RegExpr.Resolution(resolutionStringList);
+            var expected = new List<(int,int)> {
+                (1920, 1080),
+                (1024, 768),
+                (800, 600),
+                (640, 480),
+                (320, 200),
+                (320, 240),
+                (800, 600),
+                (1280, 960)};
+            Assert.Equal(expected, result);
+        }
     }
 }
