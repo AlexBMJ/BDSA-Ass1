2559f13bd3daccd875906c50c80f954f92155aa8
Alex <33891167+AlexBMJ@users.noreply.github.com>
2021-09-14 11:48:06 +0200

    optimized and shortened code (#5)

--- Assignment1.Tests/GreaterCount.cs
+++ Assignment1.Tests/GreaterCount.cs
@@ -8,7 +8,7 @@ namespace Assignment1.Tests {
             var gc = new GreaterCountTypeConstraint();
             var platypus = new Platypus(4);
             var platypus_list = new List<Platypus>() {new Platypus(5), new Platypus(2)};
-            var greater_than_count = gc.GreaterCount<Platypus, Animal>(platypus_list, platypus);
+            var greater_than_count = GreaterCountTypeConstraint.GreaterCount<Platypus, Animal>(platypus_list, platypus);
             Assert.Equal(1, greater_than_count);
         }
     }
